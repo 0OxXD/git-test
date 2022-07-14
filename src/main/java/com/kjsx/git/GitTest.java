c08f1553a2a47708c13b1fa3aa42229a87fe9390
0OxXD <824385591@qq.com>
2022-07-14 18:40:03 +0800

    idea first commit

---
+++ src/main/java/com/kjsx/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.kjsx.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: kjsx
+ * @Date: 2022/07/14/18:36
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
